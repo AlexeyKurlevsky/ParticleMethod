4d5f25a71ba1e556964d40bc023353181cc7f6f1
AlexeyKurlevsky <kurlevsky@gmail.com>
2022-11-06 21:58:35 +0300

    Add generate 2d mesh

--- ParticleMethod/ParticleMethod.cpp
+++ ParticleMethod/ParticleMethod.cpp
@@ -13,10 +13,36 @@ using namespace std;
 
 int main()
 {
-    Particle a1, a2;
+    Particle a1, a2, a3;
+    double lenghtX, lenghtY, temp_x=0, temp_y=0;
+    int numParticleX, numParticleY;
+    lenghtX = 1;
+    lenghtY = 5;
+    numParticleX = 10;
+    numParticleY = 11;
+
     a1.r = Vector2d(0, 0);
     a2.r = Vector2d(1.1, 0);
 
+    vector<Particle> particles_mesh;
+    particles_mesh.push_back(a3);
+    for (int i = 0; i < numParticleX; i++) {
+        double coordX = i * lenghtX / numParticleX;
+        for (int j = 0; j < numParticleY; j++) {
+            if (i % 2 == 0) {
+                double coordY = j * lenghtY / numParticleY;
+                a3.r = Vector2d(coordX, coordY);
+                particles_mesh.push_back(a3);
+            }
+            else {
+                double coordY = j * lenghtY / numParticleY +  lenghtY / numParticleY / 2;
+                a3.r = Vector2d(coordX, coordY);
+                particles_mesh.push_back(a3);
+            }
+        }
+    }
+
+    cout << particles_mesh.size() << endl;
     vector<Particle> particles;
     particles.push_back(a1);
     particles.push_back(a2);
@@ -26,7 +52,11 @@ int main()
     double dt = 0.01;
 
     std::ofstream myfile;
-    myfile.open("example1.csv");
+    myfile.open("D:\\plot\\mesh.csv");
+
+    for (int i = 0; i < particles_mesh.size(); i++) {
+        myfile << particles_mesh[i].r.x << "," << particles_mesh[i].r.y << "\n";
+    }
 
 
     for (int t = 0; t < time; t++) {
@@ -40,11 +70,11 @@ int main()
          cout << "Coordinate a2.r.y " << particles[1].r.y << endl;
          cout << "Force = " << F << endl;
  */
-        myfile << particles[0].r.x << "," << particles[0].r.y << "," << particles[1].r.x << "," << particles[1].r.y << "\n";
+        //myfile << particles[0].r.x << "," << particles[0].r.y << "," << particles[1].r.x << "," << particles[1].r.y << "\n";
 
     }
 
-    myfile.close();
+    //myfile.close();
     return 0;
 }
 
